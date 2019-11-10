a80820527a03cd5f73fc4f669b0d86b496ce3753
hoahtph06754 <hoahtph06754@fpt.edu.vn>
2019-11-10 23:12:20 +0700

    lab1_frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
+import { from } from 'rxjs';
+import { MenuComponent } from './menu/menu.component';
+import { ListGroupComponent } from './list-group/list-group.component';
+import { ReviewComponent } from './review/review.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent,
+    ProductComponent,
+    MenuComponent,
+    ListGroupComponent,
+    ReviewComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
